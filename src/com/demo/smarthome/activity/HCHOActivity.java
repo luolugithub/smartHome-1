7c1094e6f71234f9bdd72d32c70ead70c2769bea
leishi <shileifavorite@163.com>
2015-09-25 11:42:17 +0800

    修改获取天气信息activity的一些BUG,

--- src/com/demo/smarthome/activity/HCHOActivity.java
+++ src/com/demo/smarthome/activity/HCHOActivity.java
@@ -1,55 +1,60 @@
 package com.demo.smarthome.activity;
 
-import com.demo.smarthome.R;
-import com.demo.smarthome.activity.ScanActivity.StartUDPThread;
-import com.demo.smarthome.activity.ScanActivity.UDPThread;
-import com.demo.smarthome.device.Dev;
-import com.demo.smarthome.iprotocol.IProtocol;
-import com.demo.smarthome.protocol.MSGCMD;
-import com.demo.smarthome.protocol.MSGCMDTYPE;
-import com.demo.smarthome.protocol.Msg;
-import com.demo.smarthome.protocol.PlProtocol;
-import com.demo.smarthome.service.Cfg;
-import com.demo.smarthome.service.SocketService;
-import com.demo.smarthome.service.SocketService.SocketBinder;
-import com.demo.smarthome.tools.IpTools;
-import com.demo.smarthome.tools.StrTools;
-import com.demo.smarthome.view.SlipButton;
+		import com.demo.smarthome.R;
+		import com.demo.smarthome.activity.ScanActivity.StartUDPThread;
+		import com.demo.smarthome.activity.ScanActivity.UDPThread;
+		import com.demo.smarthome.device.Dev;
+		import com.demo.smarthome.iprotocol.IProtocol;
+		import com.demo.smarthome.protocol.MSGCMD;
+		import com.demo.smarthome.protocol.MSGCMDTYPE;
+		import com.demo.smarthome.protocol.Msg;
+		import com.demo.smarthome.protocol.PlProtocol;
+		import com.demo.smarthome.service.Cfg;
+		import com.demo.smarthome.service.SocketService;
+		import com.demo.smarthome.service.SocketService.SocketBinder;
+		import com.demo.smarthome.tools.IpTools;
+		import com.demo.smarthome.tools.StrTools;
+		import com.demo.smarthome.view.SlipButton;
 
-import android.net.wifi.WifiManager;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.HttpURLConnection;
+		import android.net.wifi.WifiManager;
+		import android.os.Bundle;
+		import android.os.Handler;
+		import android.os.IBinder;
+		import android.os.Message;
+		import android.app.Activity;
+		import android.content.BroadcastReceiver;
+		import android.content.ComponentName;
+		import android.content.Context;
+		import android.content.Intent;
+		import android.content.IntentFilter;
+		import android.content.ServiceConnection;
+		import android.util.Log;
+		import android.view.Menu;
+		import android.view.View;
+		import android.view.Window;
+		import android.view.View.OnClickListener;
+		import android.widget.Button;
+		import android.widget.ImageView;
+		import android.widget.ProgressBar;
+		import android.widget.SeekBar;
+		import android.widget.SeekBar.OnSeekBarChangeListener;
+		import android.widget.TextView;
+		import android.widget.Toast;
+		import java.io.BufferedReader;
+		import java.io.InputStreamReader;
+		import java.io.InputStream;
+		import java.net.SocketTimeoutException;
+		import java.net.URL;
+		import java.net.HttpURLConnection;
+		import java.util.concurrent.TimeoutException;
+
 /**
- * 设备显示类
- * 
- * @author Administrator
- * 
+ * 用于甲醛设备使用app
+ *
+ * @author sl
+ *
+ * create at 2015/09/23
+ *
  */
 public class HCHOActivity extends Activity {
 
@@ -71,6 +76,9 @@ public class HCHOActivity extends Activity {
 		//protected static String sunset = "";//日落时间
 	}
 
+	/**
+	 * 函数将申请到的字符串数据转换到infoReslut类中
+	 */
 	protected boolean toInfo(String jsonResult){
 		final String tempResult = jsonResult.replaceAll("\"","");//去掉所有双引号
 		String temp[];
@@ -81,26 +89,30 @@ public class HCHOActivity extends Activity {
 		if(Info.errMsg.equals("success")) {
 			temp = tempResult.split("city:");
 			Info.city = temp[1].substring(0, temp[1].indexOf(","));
+			Info.city  = decodeUnicode(Info.city);//需要从unicode转成ut8
 			temp = tempResult.split("date:");
 			Info.date = temp[1].substring(0, temp[1].indexOf(","));
 			temp = tempResult.split("time:");
 			Info.time = temp[1].substring(0, temp[1].indexOf(","));
 			temp = tempResult.split("weather:");
 			Info.weather = temp[1].substring(0, temp[1].indexOf(","));
-			//Info.weather  = new String(Info.weather.getBytes("UTF-8"), "utf-8");
+			Info.weather  = decodeUnicode(Info.weather);
 			temp = tempResult.split("l_tmp:");
 			Info.l_tmp = temp[1].substring(0, temp[1].indexOf(","));
 			temp = tempResult.split("h_tmp:");
 			Info.h_tmp = temp[1].substring(0, temp[1].indexOf(","));
 			temp = tempResult.split("WD:");
 			Info.WD = temp[1].substring(0, temp[1].indexOf(","));
+			Info.WD  = decodeUnicode(Info.WD);//需要从unicode转成ut8
 			temp = tempResult.split("WS:");
 			Info.WS = temp[1].substring(0, temp[1].indexOf(","));
+			Info.WS  = decodeUnicode(Info.WS);//需要从unicode转成ut8
 			return true;
 		}
 		return false;
 	}
-	class getEnviromentFromInternetThread extends Thread {
+
+	class getEnvironmentFromInternetThread extends Thread {
 		@Override
 		public void run() {
 			final String jsonResult = requestAPI(Cfg.WEATHER_INFORMATION, httpArg);
@@ -115,6 +127,8 @@ public class HCHOActivity extends Activity {
 		}
 	}
 	/**
+	 * 函数将从百度申请天气信息
+	 *
 	 * @param urlAll
 	 *            :请求接口
 	 * @param httpArg
@@ -132,8 +146,8 @@ public class HCHOActivity extends Activity {
 					.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setConnectTimeout(10000);
-			// 填入apikey到HTTP header
-			connection.setRequestProperty("apikey",  "63cb0c1770c622f9287d62868c079989");
+			// 填入apikey到HTTP header,其中apikey是绑定百度账号的
+			connection.setRequestProperty("apikey",Cfg.APIKEY);
 			connection.connect();
 			InputStream is = connection.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
@@ -145,7 +159,11 @@ public class HCHOActivity extends Activity {
 
 			reader.close();
 			result = sbf.toString();
-		} catch (Exception e) {
+		}
+		catch (SocketTimeoutException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
@@ -167,8 +185,9 @@ public class HCHOActivity extends Activity {
 			}
 
 		});
-	Button getEnviroment = (Button) findViewById(R.id.getEnvironment);
-	getEnviroment.setOnClickListener(new getEnviromentListener());
+
+		Button getEnviroment = (Button) findViewById(R.id.getEnvironment);
+		getEnviroment.setOnClickListener(new getEnviromentListener());
 	}
 
 	class getEnviromentListener implements OnClickListener {
@@ -177,7 +196,80 @@ public class HCHOActivity extends Activity {
 		public void onClick(View v) {
 			TextView environmentInfo = (TextView)findViewById(R.id.enviInfoTextView);
 			environmentInfo.setText("请稍等，信息正在获取中");
-			new getEnviromentFromInternetThread().start();
+			new getEnvironmentFromInternetThread().start();
 		}
+	}
+	/**
+	 * 函数用于将unicode转换成utf-8
+	 * @param String theString
+	 *            :unicode
+	 * @return String
+	 *			:UTF-8
+	 *
+	 */
+	private static String decodeUnicode(String theString) {
+		char aChar;
+		int len = theString.length();
+		StringBuffer outBuffer = new StringBuffer(len);
+		for (int x = 0; x < len;) {
+			aChar = theString.charAt(x++);
+			if (aChar == '\\') {
+				aChar = theString.charAt(x++);
+				if (aChar == 'u') {
+					// Read the xxxx
+					int value = 0;
+					for (int i = 0; i < 4; i++) {
+						aChar = theString.charAt(x++);
+						switch (aChar) {
+							case '0':
+							case '1':
+							case '2':
+							case '3':
+							case '4':
+							case '5':
+							case '6':
+							case '7':
+							case '8':
+							case '9':
+								value = (value << 4) + aChar - '0';
+								break;
+							case 'a':
+							case 'b':
+							case 'c':
+							case 'd':
+							case 'e':
+							case 'f':
+								value = (value << 4) + 10 + aChar - 'a';
+								break;
+							case 'A':
+							case 'B':
+							case 'C':
+							case 'D':
+							case 'E':
+							case 'F':
+								value = (value << 4) + 10 + aChar - 'A';
+								break;
+							default:
+								throw new IllegalArgumentException(
+										"Malformed   \\uxxxx   encoding.");
+						}
+
+					}
+					outBuffer.append((char) value);
+				} else {
+					if (aChar == 't')
+						aChar = '\t';
+					else if (aChar == 'r')
+						aChar = '\r';
+					else if (aChar == 'n')
+						aChar = '\n';
+					else if (aChar == 'f')
+						aChar = '\f';
+					outBuffer.append(aChar);
+				}
+			} else
+				outBuffer.append(aChar);
+		}
+		return outBuffer.toString();
 	}
 }
